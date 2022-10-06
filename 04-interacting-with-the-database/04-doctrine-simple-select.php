288c355e84169674759e244b4e50e6a575fe21ac
uhajzeraj <uhajzeraj@hotmail.com>
2022-10-06 23:11:04 +0200

    Create 04-doctrine-simple-select.php

---
+++ 04-interacting-with-the-database/04-doctrine-simple-select.php
@@ -0,0 +1,37 @@
+<?php
+
+use Doctrine\DBAL\DriverManager;
+
+require 'vendor/autoload.php';
+
+$connectionParams = [
+    'dbname' => 'flossk_demo',
+    'user' => 'root',
+    'password' => '',
+    'host' => 'localhost',
+    'driver' => 'pdo_mysql',
+];
+
+$connection = DriverManager::getConnection($connectionParams);
+
+$sql = 'SELECT * FROM users';
+
+$result = $connection->executeQuery($sql);
+
+while (($row = $result->fetchAssociative()) !== false) {
+    dump($row);
+}
+
+//==============================================================================
+
+// $sql = 'SELECT * FROM users WHERE role = :role';
+// $statement = $connection->prepare($sql);
+// $statement->bindValue('role', 'admin');
+
+// $result = $statement->executeQuery();
+
+// $users = $result->fetchAllAssociative();
+
+// foreach ($users as $user) {
+//     dump($user);
+// }
