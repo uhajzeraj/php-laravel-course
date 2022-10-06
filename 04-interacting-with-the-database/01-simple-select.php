08a668ca0bf25360be188dca9d9c748d044ed06f
uhajzeraj <uhajzeraj@hotmail.com>
2022-10-06 22:20:29 +0200

    PDO::FETCH_ASSOC

--- 04-interacting-with-the-database/01-simple-select.php
+++ 04-interacting-with-the-database/01-simple-select.php
@@ -6,11 +6,13 @@ $user = 'root';
 $pass = '';
 
 try {
-    $dbh = new PDO('mysql:host=localhost;dbname=flossk_demo', $user, $pass);
-    foreach ($dbh->query('SELECT * FROM users LIMIT 1') as $row) {
+    $connection = new PDO('mysql:host=localhost;dbname=flossk_demo', $user, $pass);
+
+    foreach ($connection->query('SELECT * FROM users LIMIT 1', PDO::FETCH_ASSOC) as $row) {
         dump($row);
     }
-    $dbh = null;
+
+    $connection = null;
 } catch (PDOException $e) {
     print $e->getMessage() . PHP_EOL;
     die();
